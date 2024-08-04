ed5968d4659c557689f10b54f06f55d1e7ae7129
zerogravitii <jerwinchan@gmail.com>
2024-08-04 09:18:50 -0400

    added registration

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -27,6 +27,7 @@ const giftRoutes = require('./routes/giftRoutes');
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 const searchRoutes = require('./routes/searchRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
@@ -39,6 +40,7 @@ app.use('/api/gifts', giftRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 app.use('/api/search', searchRoutes);
+app.use('/api/auth', authRoutes);
 
 
 // Global Error Handler
---
+++ giftlink-backend/routes/authRoutes.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { body, validationResult } = require('express-validator');
+const connectToDatabase = require('../models/db');
+const dotenv = require('dotenv');
+const pino = require('pino');  // Import Pino logger
+
+const logger = pino();
+
+dotenv.config();
+
+const app = express();
+const router = express.Router();
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+router.post('/register', async (req, res) => {
+    try {
+        // Task 1: Connect to `giftsdb` in MongoDB through `connectToDatabase` in `db.js`
+        const db = await connectToDatabase();
+        // Task 2: Access MongoDB collection
+        const collection = db.collection("users");
+        //Task 3: Check for existing email
+        const existingEmail = await collection.findOne({ email: req.body.email });
+
+
+        const salt = await bcryptjs.genSalt(10);
+        const hash = await bcryptjs.hash(req.body.password, salt);
+        const email = req.body.email;
+
+        const newUser = await collection.insertOne({
+            email: req.body.email,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            password: hash,
+            createdAt: new Date(),
+        });
+
+        const payload = {
+            user: {
+                id: newUser.insertedId,
+            },
+        };
+
+        const authtoken = jwt.sign(payload, JWT_SECRET);
+
+        //Task 5: Create JWT authentication with user._id as payload
+        logger.info('User registered successfully');
+        res.json({authtoken,email});
+    } catch (e) {
+         return res.status(500).send('Internal server error');
+    }
+});
+
+module.exports = router;
