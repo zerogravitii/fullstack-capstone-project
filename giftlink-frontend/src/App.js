461788817f570f34c000f51bc13bdb4d87d356e4
zerogravitii <jerwinchan@gmail.com>
2024-08-05 23:13:15 +0800

    Update App.js

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -10,7 +10,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
     return (
         <>
             <Navbar/>
